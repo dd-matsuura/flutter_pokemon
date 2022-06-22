f2338fc016fe42f549e7cb3b7929425790ae3c96
dd-matsuura <matsuura.t@docodoor.co.jp>
2022-06-23 01:07:14 +0900

    Zennに記載のない変更を反映

--- lib/main.dart
+++ lib/main.dart
@@ -24,9 +24,13 @@ class TopPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
-          itemCount: 10000,
-          itemBuilder: (context, index) => PokeListItem(index: index)),
+      body: SafeArea(
+        child: ListView.builder(
+          padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 16),
+          itemCount: 898,
+          itemBuilder: (context, index) => PokeListItem(index: index),
+        ),
+      ),
     );
   }
 }
