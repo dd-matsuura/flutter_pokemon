204eef58fb97b16cc510981513d879143567f6db
dd-matsuura <matsuura.t@docodoor.co.jp>
2022-06-23 01:32:08 +0900

    Step6: 設定を保存してみよう - 設定画面の作成 - 設定の状態を共有しよう

--- lib/main.dart
+++ lib/main.dart
@@ -68,17 +68,107 @@ class PokeList extends StatelessWidget {
   }
 }
 
-class Settings extends StatelessWidget {
+class Settings extends StatefulWidget {
   const Settings({Key? key}) : super(key: key);
+
+  @override
+  State<Settings> createState() => _SettingsState();
+}
+
+class _SettingsState extends State<Settings> {
+  ThemeMode _themeMode = ThemeMode.system;
   @override
   Widget build(BuildContext context) {
     return ListView(
-      children: const [
+      children: [
         ListTile(
-          leading: Icon(Icons.lightbulb),
-          title: Text('Dark/Light Mode'),
+          leading: const Icon(Icons.lightbulb),
+          title: const Text('Dark/Light Mode'),
+          trailing: Text((_themeMode == ThemeMode.system)
+              ? 'System'
+              : (_themeMode == ThemeMode.dark ? 'Dark' : 'Light')),
+          onTap: () async {
+            final ret = await Navigator.of(context).push<ThemeMode>(
+              MaterialPageRoute(
+                builder: (context) => ThemeModeSelectionPage(init: _themeMode),
+              ),
+            );
+            setState(() => _themeMode = ret!);
+          },
+        ),
+        SwitchListTile(
+          title: const Text('Switch'),
+          value: true,
+          onChanged: (yes) => {},
+        ),
+        CheckboxListTile(
+          title: const Text('Checkbox'),
+          value: true,
+          onChanged: (yes) => {},
+        ),
+        RadioListTile(
+          title: const Text('Radio'),
+          value: true,
+          groupValue: true,
+          onChanged: (yes) => {},
         ),
       ],
+    );
+  }
+}
+
+class ThemeModeSelectionPage extends StatefulWidget {
+  const ThemeModeSelectionPage({
+    Key? key,
+    required this.init,
+  }) : super(key: key);
+  final ThemeMode init;
+
+  @override
+  State<ThemeModeSelectionPage> createState() => _ThemeModeSelectionPageState();
+}
+
+class _ThemeModeSelectionPageState extends State<ThemeModeSelectionPage> {
+  late ThemeMode _current;
+  @override
+  void initState() {
+    super.initState();
+    _current = widget.init;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            ListTile(
+              leading: IconButton(
+                icon: const Icon(Icons.arrow_back),
+                onPressed: () => Navigator.pop<ThemeMode>(context, _current),
+              ),
+            ),
+            RadioListTile<ThemeMode>(
+              value: ThemeMode.system,
+              groupValue: _current,
+              title: const Text('System'),
+              onChanged: (val) => {setState(() => _current = val!)},
+            ),
+            RadioListTile<ThemeMode>(
+              value: ThemeMode.dark,
+              groupValue: _current,
+              title: const Text('Dark'),
+              onChanged: (val) => {setState(() => _current = val!)},
+            ),
+            RadioListTile<ThemeMode>(
+              value: ThemeMode.light,
+              groupValue: _current,
+              title: const Text('Light'),
+              onChanged: (val) => {setState(() => _current = val!)},
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
