59133de25db5004cfb1c0211e0ca76c1c943ab38
dd-matsuura <matsuura.t@docodoor.co.jp>
2022-06-24 14:31:27 +0900

    Step6 : SharedPreferencesで起動時テーマモード保持（ホットリスタート切替）

--- lib/main.dart
+++ lib/main.dart
@@ -6,15 +6,29 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  ThemeMode themeMode = ThemeMode.system;
+
+  @override
+  void initState() {
+    super.initState();
+    loadThemeMode().then((val) => setState(() => themeMode = val));
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Pokemon Flutter',
       theme: ThemeData.light(),
       darkTheme: ThemeData.dark(),
-      // themeMode: mode,
+      themeMode: themeMode,
       home: const TopPage(),
     );
   }
