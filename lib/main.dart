f744dd44e369867fc4c412b5aa3b46e6cc14bb43
dd-matsuura <matsuura.t@docodoor.co.jp>
2022-06-24 14:41:27 +0900

    Setting画面を別ページに記述

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_pokemon/poke_list_item.dart';
+import 'package:flutter_pokemon/setting.dart';
 import 'package:flutter_pokemon/theme_mode.dart';
 
 void main() {
@@ -79,120 +80,6 @@ class PokeList extends StatelessWidget {
       padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 16),
       itemCount: 898,
       itemBuilder: (context, index) => PokeListItem(index: index),
-    );
-  }
-}
-
-class Settings extends StatefulWidget {
-  const Settings({Key? key}) : super(key: key);
-
-  @override
-  State<Settings> createState() => _SettingsState();
-}
-
-class _SettingsState extends State<Settings> {
-  ThemeMode _themeMode = ThemeMode.system;
-
-  @override
-  void initState() {
-    super.initState();
-    // loadThemeMode()が呼ばれた後に、thenを実行してsetStateで処理を実行
-    loadThemeMode().then((val) => setState(() => _themeMode = val));
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return ListView(
-      children: [
-        ListTile(
-          leading: const Icon(Icons.lightbulb),
-          title: const Text('Dark/Light Mode'),
-          trailing: Text((_themeMode == ThemeMode.system)
-              ? 'System'
-              : (_themeMode == ThemeMode.dark ? 'Dark' : 'Light')),
-          onTap: () async {
-            final ret = await Navigator.of(context).push<ThemeMode>(
-              MaterialPageRoute(
-                builder: (context) => ThemeModeSelectionPage(init: _themeMode),
-              ),
-            );
-            setState(() => _themeMode = ret!);
-            await saveThemeMode(_themeMode);
-          },
-        ),
-        SwitchListTile(
-          title: const Text('Switch'),
-          value: true,
-          onChanged: (yes) => {},
-        ),
-        CheckboxListTile(
-          title: const Text('Checkbox'),
-          value: true,
-          onChanged: (yes) => {},
-        ),
-        RadioListTile(
-          title: const Text('Radio'),
-          value: true,
-          groupValue: true,
-          onChanged: (yes) => {},
-        ),
-      ],
-    );
-  }
-}
-
-class ThemeModeSelectionPage extends StatefulWidget {
-  const ThemeModeSelectionPage({
-    Key? key,
-    required this.init,
-  }) : super(key: key);
-  final ThemeMode init;
-
-  @override
-  State<ThemeModeSelectionPage> createState() => _ThemeModeSelectionPageState();
-}
-
-class _ThemeModeSelectionPageState extends State<ThemeModeSelectionPage> {
-  late ThemeMode _current;
-  @override
-  void initState() {
-    super.initState();
-    _current = widget.init;
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Column(
-          children: [
-            ListTile(
-              leading: IconButton(
-                icon: const Icon(Icons.arrow_back),
-                onPressed: () => Navigator.pop<ThemeMode>(context, _current),
-              ),
-            ),
-            RadioListTile<ThemeMode>(
-              value: ThemeMode.system,
-              groupValue: _current,
-              title: const Text('System'),
-              onChanged: (val) => {setState(() => _current = val!)},
-            ),
-            RadioListTile<ThemeMode>(
-              value: ThemeMode.dark,
-              groupValue: _current,
-              title: const Text('Dark'),
-              onChanged: (val) => {setState(() => _current = val!)},
-            ),
-            RadioListTile<ThemeMode>(
-              value: ThemeMode.light,
-              groupValue: _current,
-              title: const Text('Light'),
-              onChanged: (val) => {setState(() => _current = val!)},
-            ),
-          ],
-        ),
-      ),
     );
   }
 }
